bc3d5a00f9d7450f109f162bfbc786ebabfe6aff
erikw <erik.wernersson@scilifelab.se>
2018-06-01 09:03:54 +0200

    ...

--- common/cCorr/cCorrFolder_gui.m
+++ common/cCorr/cCorrFolder_gui.m
@@ -1,9 +1,27 @@
 function cCorrFolder_gui(varargin)
+%% cCorrFolder_gui
+%
+% Correct images from shifts and chromatic aberrations measured from beads.
+%
+% 1. Select folder(s) that contains TIFF images.
+% 2. Select the correction file to use
+%
+% For each input folder, the corrected images will be created in a new 
+% subfolder called cc.
+%
+% As default, 'DAPI' is the reference channel, i.e., 'DAPI' is left
+% unchanged while the other channels are deformed to map 'DAPI'.
+% If 'DAPI' does not extist in the beads data, you will be queried to use
+% another reference channel.
+%
+% Make sure not to correct the dots that you get from the images in the
+% cc-folder since applying the correction twice will give you at least as 
+% bad results as us you would get using no correction at all.
 
 s.inFolder = df_getConfig('cCorrFolder_gui', 'inFolder', '~/Desktop/');
 s.ccFile = df_getConfig('cCorrFolder_gui', 'ccFile', '~/Desktop/test.cc');
 
-GUI.fig = figure('Position', [0,200,400,600], 'Menubar', 'none', ...    
+GUI.fig = figure('Position', [0,200,400,600], 'Menubar', 'none', ...
     'NumberTitle','off', ... % 'Color', [.8,1,.8], ...
     'Name', 'CC on folder');
 
@@ -60,19 +78,22 @@ GUI.outFolder = uicontrol(GUI.fig, 'Style', 'text', ...
 % Controls
 GUI.start = uicontrol(GUI.fig, 'Style', 'pushbutton', ...
     'String', 'Start', ...
-    'Position',[200 100 150 30], ...
+    'Units', 'Normalized', ...
+    'Position',[.7, 0, .3, .1], ...
     'Enable', 'off', ...
     'Callback', @gui_run);
 
-GUI.pickOutFolder = uicontrol(GUI.fig, 'Style', 'pushbutton', ...
+GUI.cancel = uicontrol(GUI.fig, 'Style', 'pushbutton', ...
     'String', 'Cancel', ...
-    'Position',[30 100 150 30], ...
+    'Units', 'Normalized', ...
+    'Position',[0, 0, .3, .1], ...
     'Callback', @gui_quit);
 
-GUI.pickOutFolder = uicontrol(GUI.fig, 'Style', 'pushbutton', ...
-    'String', 'debug', ...
-    'Position',[0 0 50 30], ...
-    'Callback', @gui_debug);
+GUI.help = uicontrol(GUI.fig, 'Style', 'pushbutton', ...
+    'String', 'Help', ...
+    'Units', 'Normalized', ...
+    'Position',[0.4, 0, .3, .1], ...
+    'Callback', @gui_help);
 
 
 gui_refresh();
@@ -82,21 +103,25 @@ try
     close(GUI.fig);
 end
 
+    function gui_help(varargin)
+        hstring = help('cCorrFolder_gui');
+        msgbox(hstring);
+    end
 
-function gui_selectInFolder(varargin)
+    function gui_selectInFolder(varargin)
         disp('select folder')
         t = uipickfiles('FilterSpec', s.inFolder);
         if ~isnumeric(t)
             s.inFolder = t{1};
             s.outFolder = [s.inFolder '/cc'];
-            df_setConfig('cCorrFolder_gui', 'inFolder', s.inFolder);            
+            df_setConfig('cCorrFolder_gui', 'inFolder', s.inFolder);
             gui_refresh();
         end
-end
+    end
 
-function gui_selectCCfile(varargin)
+    function gui_selectCCfile(varargin)
         disp('select ccFile');
-
+        
         [folder, ~, ~] = fileparts(s.ccFile);
         t = uipickfiles('FilterSpec', [folder '/*.cc']);
         if ~isnumeric(t)
@@ -104,7 +129,7 @@ function gui_selectCCfile(varargin)
             df_setConfig('cCorrFolder_gui', 'ccFile', s.ccFile);
             gui_refresh();
         end
-end
+    end
 
     function gui_quit(varargin)
         uiresume();
@@ -120,18 +145,18 @@ end
         set(GUI.ccFile, 'String', s.ccFile);
         
         if 1
-         set(GUI.start, 'Enable', 'on');
+            set(GUI.start, 'Enable', 'on');
         end
         
         if( exist(s.ccFile, 'file') )
             %set(GUI.ccFile, 'te');
-        end                
+        end
     end
 
     function gui_run(varargin)
-       fprintf('cCorrFolder2(%s, %s)\n', s.inFolder, s.ccFile);
-       df_cc_apply_image_folder(s.inFolder, s.ccFile);
-       %cCorrFolder2(s.inFolder, s.ccFile);
+        fprintf('cCorrFolder2(%s, %s)\n', s.inFolder, s.ccFile);
+        df_cc_apply_image_folder(s.inFolder, s.ccFile);
+        %cCorrFolder2(s.inFolder, s.ccFile);
     end
 
 end
