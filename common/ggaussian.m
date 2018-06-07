53a22102942ee5555525ae700d48d7114659f0cf
erikw <erik.wernersson@scilifelab.se>
2018-06-07 16:25:08 +0200

    ...

--- common/ggaussian.m
+++ common/ggaussian.m
@@ -6,12 +6,18 @@ assert(mod(size,2)==1);
 s=(size-1)/2;
 
 D=-s:s;
-
 mu = 0;
-%g=normpdf(D, 0, sigma); % Requires statistics toolbox
-g = 1/(sigma*sqrt(2*pi))*exp(-(D-mu).^2/(sigma*sqrt(2)));
+
+f = @(x) gauss1(x, mu, sigma);
+
+% Redo with erf
+for kk = 1:numel(D)
+    g(kk) = integral(f, D(kk)-.5, D(kk)+.5);
+end
 
 g=g./sum(g);
+end
 
-
-
+function g = gauss1(D, mu, sigma)
+    g = 1/(sigma*sqrt(2*pi))*exp(-(D-mu).^2/(sigma^2*2));
+end
---
+++ common/ggaussian_ut.m
@@ -0,0 +1,30 @@
+function ggaussian_ut()
+
+g1 = ggaussian(15, 1);
+g2 = fspecial('gaussian', [15,1],1);
+D = -7:7;
+g3 = normpdf(D, 0, 1);
+oversampling = 100;
+g10 = normpdf(linspace(-7.5, 7.5, 15*oversampling));
+g4 = imresize(g10, 1/10); 
+g4 = g4/sum(g4);
+g5 = reshape(g10, [oversampling,15]);
+g5 = sum(g5,1);
+g5 = g5/sum(g5);
+
+sum(g1)
+sum(g2)
+assert(abs(1-sum(abs(g1)))<1e-9)
+
+if(0)
+    figure,
+    plot(g1, 'k')
+    hold on
+    plot(g2, '--g')
+    plot(g3, ':r')
+    plot(g5, 'ro')
+    legend({'ggaussian', 'fspecial', 'normpdf', 'resized normpdf'})
+    dprintpdf('gaussians.pdf')
+end
+
+end
