cd599aed63958881d191246eee1880d0802aa579
erikw <erik.wernersson@scilifelab.se>
2018-09-26 14:49:59 +0200

    More comments

--- common/mex/blit3.h
+++ common/mex/blit3.h
@@ -9,12 +9,32 @@ enum _photonw {
 typedef enum _photonw photonw;
 
 double rand_range(double , double );
-void blit3(double * , uint32_t , uint32_t , uint32_t ,
-    double * , uint32_t , uint32_t , uint32_t ,
-    int64_t , int64_t , int64_t ,
-    int8_t );
-int blit3g(double * , uint32_t , uint32_t , uint32_t ,
-    double * , size_t , photonw, int);
+
+/* Place 3D Gaussian shapes defined by D into the volumetric image T
+ * photonw says if the 7th column of D specifies the total number 
+ * of photons, the number of photons in the plane or the number of
+ * photons in the central pixel. I.e., how the Gaussian should be scaled.
+ * Supports sub pixel precision coordinates. 
+ */
+
+int blit3g(double * T, // Target image
+    uint32_t Tm, uint32_t Tn, uint32_t Tp, // Rows, Columns, Slices of T
+    double * D,  // Table with point to blit, x, y, z, nphot, sigmax, sigmay, sigmaz
+    size_t nD, // Number of points
+    photonw pw, // says how nphot should be interpreted
+    int one_indexing); // set to 0 if the coordinates in D are 0-indexed. 1 if 1-indexed
+
+/* Similar to blit3g, however, in this case a volumetric image defined by
+ * S is blitted to the coordinate given by (Pm, Pn, Pp)
+ * Possible values for anchor:
+ * 0 = corner
+ * 1 = centered over P (not supported yet)
+ */
+void blit3(double * T, uint32_t Tm, uint32_t Tn, uint32_t Tp,
+    double * S, uint32_t Sm, uint32_t Sn, uint32_t Sp,
+    int64_t Pm, int64_t Pn, int64_t Pp,
+    int8_t anchor);
+ 
 int unit_tests(void);
 
 #endif
