f0764c4bc63c6ab6b99ba72330bf73e9afe7ca92
elgw <erik.wernersson@gmail.com>
2020-06-25 10:13:05 +0200

    Circumventing the whimsical writetable

---
+++ common/df_writeTable.m
@@ -0,0 +1,48 @@
+function df_writeTable(tab, fname)
+% Similar like writetable but overcomes the issue that
+% values sometimes are rounded
+% However this function is more limited in terms of functionality
+% Only supports {'char'} and 'double' cells.
+
+props = tab.Properties.VariableNames;
+
+fid = fopen(fname, "w");
+for kk = 1:numel(props)-1
+    fprintf(fid, '%s,', props{kk});
+end
+fprintf(fid, '%s\n', props{end});
+
+
+propsClass = {};
+for kk = 1:size(tab, 2)
+    pcell = tab{2,kk};
+    if isa(pcell, 'cell')
+        propsClass{kk} = class(pcell{1});
+    else
+        propsClass{kk} = class(pcell);
+    end
+end
+
+nCols = numel(propsClass);
+for kk = 2:size(tab,1)
+    for pp = 1:nCols-1
+        writeCell(fid, tab{kk,pp}, propsClass{pp});
+        fprintf(fid, ',');
+    end
+    writeCell(fid, tab{kk,nCols}, propsClass{nCols});
+    fprintf(fid, '\n');    
+end
+
+fclose(fid);
+
+end
+
+function writeCell(fid, acell, pclass)
+if( strcmp(pclass, 'char') )
+    fprintf(fid, '%s', acell{1});
+end
+if( strcmp(pclass, 'double') )
+    fprintf(fid, '%.3f', acell);
+end
+
+end
