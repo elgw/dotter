717481a9b75522c357b989390939c2626ec8f43b
Erik Wernersson <erik.wernersson@gmail.com>
2023-08-24 15:05:11 +0200

    Removed unused files

--- patches/dotter_patch_20171201.m
+++
@@ -1,8 +0,0 @@
-% TMR channel partially missing in
-
-folder = '/mnt/bicroserver2/microscopy_data_2/iFISH/iEG436_171125_003_calc/';
-
-files = dir([folder '*.NM']);
-
-D = load([folder files(1).name], '-mat');
-M = D.M
--- scripts/df_cc_apply_image_folder.m
+++ scripts/df_cc_apply_image_folder.m
@@ -18,7 +18,7 @@ else
 end
 
 logFileName = sprintf('%scc_log.txt', outDir);
-if isfile(logFileName)    
+if isfile(logFileName)
     errordlg('A log file already exist in the input folder which suggests that it has been corrected already');
     return
 end
@@ -38,18 +38,18 @@ fprintf('Reference channel: %s\n', refChannel);
 
 
 w = waitbar(0, 'Processing ...');
-for kk = 1:numel(files)    
-    
+for kk = 1:numel(files)
+
     fprintf('(%d/%d) %s\n', kk, numel(files), files(kk).name);
 	I = df_readTif([folder files(kk).name]);
     channel = im2chan_name(files(kk).name);
-    cn = find(strcmp(cc.channels, channel));        
+    cn = find(strcmp(cc.channels, channel));
     if sum(strcmp(cc.channels, channel{1})) ~= 1
         msgbox(sprintf('Can''t correct channel %s, the cc files does not contain any information about it!', channel{1}));
         return
     end
-    C = cc_apply_image('image', I, 'ccData', cc, 'from', channel{1}, 'to', refChannel);
-    df_writeTif(uint16(C), [outDir files(kk).name]);    
+    C = df_cc_apply_image('image', I, 'ccData', cc, 'from', channel{1}, 'to', refChannel);
+    df_writeTif(uint16(C), [outDir files(kk).name]);
     waitbar(kk/numel(files), w);
 end
 close(w)
@@ -66,23 +66,23 @@ refChannel = '';
 
 hasRefChannel = find(strcmpi(cc.channels, defChan));
 if numel(hasRefChannel) == 0
-    
+
     PromptString{1} = 'The default reference channel';
     PromptString{2} = sprintf('%s does not exist', defChan);
     PromptString{3} = 'please use another one';
     PromptString{4} = '';
-    
+
     refChanNo = listdlg('ListString', cc.channels, ...
         'SelectionMode', 'single', ...
         'PromptString', PromptString);
-    
-    
+
+
     if numel(refChanNo) == 1
         refChannel = cc.channels{refChanNo};
     else
         error('No reference channel picked');
     end
-    
+
 
 else
     refChannel = defChan;
